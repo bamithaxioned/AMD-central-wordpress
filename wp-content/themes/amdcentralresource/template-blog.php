780dc132968c2f2e29041ef0bbb39cb908eecdc7
bamithprdxn <amithkumarb@prdxn.com>
2021-03-08 13:13:51 +0530

    Modifed Index.php and template for blog

---
+++ wp-content/themes/amdcentralresource/template-blog.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+	<?php
+		$bodyclass = get_body_class();
+
+		if (in_array('blog', $bodyclass)) {
+
+			$displayBlogArgs = array(
+				'post_type' => 'sports',
+				'posts_per_page' => '-1'
+			);
+
+			$displayBlog = new Wp_Query($displayBlogArgs);
+
+
+			if ($displayBlog->have_posts()) : ?>
+				<section class="display_latest_post">
+					<div class="wrapper">
+						<h2 class="recent_post center">Posts</h2>
+						<ul class="latest_post">
+				<?php while($displayBlog->have_posts()) : $displayBlog->the_post() ?>
+					<li class="latest_post_list">
+						<h3 class="latest_post_heading"><?php the_title(); ?></h3>
+						<p class="latest_post_content"><?php the_excerpt(); ?></p>
+					<a href="<?php the_permalink(); ?>" class="read_btn" title="Read More">Read More</a>
+					</li>
+			<?php endwhile; ?>
+					</ul>
+				</div>
+			</section>
+			<?php endif;
+		}
+	?>
+
+
+<?php get_footer(); ?>
