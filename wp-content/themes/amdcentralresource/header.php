5cc0f416e18125303775cac76acd13f14dc64b29
bamithprdxn <amithkumarb@prdxn.com>
2021-03-08 11:34:28 +0530

    modified header.php-  for displaying menus

--- wp-content/themes/amdcentralresource/header.php
+++ wp-content/themes/amdcentralresource/header.php
@@ -13,5 +13,13 @@
 	<header class="header">
 		<div class="wrapper">
 			<h1 class="header_heading"><a href="<?php get_home_url(); ?>" title="<?php echo $siteTitle; ?>" class="header_heading_link"><?php echo $siteTitle; ?></a></h1>
+			<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'top-menu',
+						'menu_class' => 'navigation'
+					)
+				);
+			?>
 		</div>
 	</header>
