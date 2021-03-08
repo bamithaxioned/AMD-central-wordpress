ddf64489dd3934577abc395d6266505afa5d8031
bamithprdxn <amithkumarb@prdxn.com>
2021-03-08 13:10:10 +0530

    Header.php modified

--- wp-content/themes/amdcentralresource/header.php
+++ wp-content/themes/amdcentralresource/header.php
@@ -12,7 +12,7 @@
 <body>
 	<header class="header">
 		<div class="wrapper">
-			<h1 class="header_heading"><a href="<?php get_home_url(); ?>" title="<?php echo $siteTitle; ?>" class="header_heading_link"><?php echo $siteTitle; ?></a></h1>
+			<h1 class="header_heading"><a href="<?php echo get_home_url(); ?>" title="<?php echo $siteTitle; ?>" class="header_heading_link"><?php echo $siteTitle; ?></a></h1>
 			<?php
 				wp_nav_menu(
 					array(
