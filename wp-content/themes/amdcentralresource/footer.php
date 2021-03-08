00c8634328bcf79714f2dab098aa816dfff1f375
bamithprdxn <amithkumarb@prdxn.com>
2021-03-08 11:36:46 +0530

    modified footer.php - for displaying menus

--- wp-content/themes/amdcentralresource/footer.php
+++ wp-content/themes/amdcentralresource/footer.php
@@ -6,6 +6,14 @@
 			<div class="wrapper">
 				<h6 class="footer_heading"><a href="<?php echo get_home_url(); ?>" title="<?php echo 
 				$siteTitle; ?>" class="footer_heading_link"><?php echo $siteTitle; ?></a></h6>
+				<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'footer-menu',
+							'menu_class' => 'navigation',
+						)
+					);
+				?>
 			</div>
 		</footer>
 		<?php wp_footer(); ?>
