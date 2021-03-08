bb0c6b92d6068bbe5e8ef048d25f3eb855a400ef
bamithprdxn <amithkumarb@prdxn.com>
2021-03-08 11:05:11 +0530

    Code for footer section added

---
+++ wp-content/themes/amdcentralresource/footer.php
@@ -0,0 +1,13 @@
+<?php
+	$siteTitle = get_bloginfo('name');
+	$siteTagLine = get_bloginfo('description');
+?>
+		<footer class="footer">
+			<div class="wrapper">
+				<h6 class="footer_heading"><a href="<?php echo get_home_url(); ?>" title="<?php echo 
+				$siteTitle; ?>" class="footer_heading_link"><?php echo $siteTitle; ?></a></h6>
+			</div>
+		</footer>
+		<?php get_footer(); ?>
+	</body>
+</html>
