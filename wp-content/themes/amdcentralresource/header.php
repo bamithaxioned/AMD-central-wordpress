31f67424c007e9ffdbe64c9b5410a9b28f044a5b
bamithprdxn <amithkumarb@prdxn.com>
2021-03-08 10:47:46 +0530

    Code for header file added

---
+++ wp-content/themes/amdcentralresource/header.php
@@ -0,0 +1,19 @@
+<?php
+	$siteTitle = get_bloginfo('name');
+	$siteTagLine =  get_bloginfo('description');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $siteTitle ." - " . $siteTagLine; ?></title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<header class="header">
+		<div class="wrapper">
+			<h1 class="header_heading"><a href="<?php get_home_url(); ?>" title="<?php echo $siteTitle; ?>" class="header_heading_link"><?php echo $siteTitle; ?></a></h1>
+		</div>
+	</header>
+</body>
+</html>
