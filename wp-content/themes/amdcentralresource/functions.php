f50c85b5d51671c1ae30ebf4fce1b9127b814fc6
bamithprdxn <amithkumarb@prdxn.com>
2021-03-08 11:33:43 +0530

    Function for displaying menus added

--- wp-content/themes/amdcentralresource/functions.php
+++ wp-content/themes/amdcentralresource/functions.php
@@ -6,4 +6,17 @@
 		wp_enqueue_script('custom-js', get_template_directory_uri() . '/js/amdjs.js', array(), '1.0.0', true);
 	};
 	add_action('wp_enqueue_scripts', 'amd_load_scripts');
+
+	#ADDING MENUS
+	function amd_display_menus()
+	{
+		add_theme_support('menus');
+		register_nav_menu(
+			array(
+				'top-menu' => "Header Menu",
+				'footer-menu' => 'Footer Menu'
+			)
+		);
+	};
+	add_action('init', 'amd_display_menus');
 ?>
